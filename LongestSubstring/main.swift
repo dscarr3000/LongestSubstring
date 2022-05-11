add19e5377803fd91449ecb5d79989d082028445
Daniel Carr <54419206+dscarr3000@users.noreply.github.com>
2022-05-10 20:24:23 -0600

    Created solution

--- LongestSubstring/main.swift
+++ LongestSubstring/main.swift
@@ -1,11 +1,27 @@
-//
-//  main.swift
-//  LongestSubstring
-//
-//  Created by Daniel Carr on 5/10/22.
-//
-
 import Foundation
 
-print("Hello, World!")
+func lengthOfLongestSubstring(_ s: String) -> Int {
+    var chars: [Character: Int] = [:]
+    var left = 0
+    var right = 0
+    var res = 0
+    
+    while right < s.count {
+        let r = s[s.index(s.startIndex, offsetBy: right)]
+        chars[r, default: 0] += 1
+        
+        while chars[r, default: 0] > 1 {
+            let l = s[s.index(s.startIndex, offsetBy: left)]
+            chars[l, default: 0] -= 1
+            left += 1
+        }
+        
+        res = max(res, right - left + 1)
+        right += 1
+    }
+    
+    return res
+}
+
+print(lengthOfLongestSubstring("abcabca"))
 
